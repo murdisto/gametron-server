72482fcfdcd392884d0a4fb5a082131bb56477d8
Scott Ian <sim2487@gmail.com>
2021-01-30 09:24:18 -0800

    Cors testing

--- index.js
+++ index.js
@@ -9,34 +9,33 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const { router: userRouter } = require('./users');
-const {router: gameRouter} = require('./games');
-const {router: postRouter} = require('./posts');
-const {router: imageRouter} = require('./images/router');
- 
-const {router: emailRouter} = require('./email');
+const { router: gameRouter } = require('./games');
+const { router: postRouter } = require('./posts');
+const { router: imageRouter } = require('./images/router');
+
+const { router: emailRouter } = require('./email');
 
 const app = express();
 const jsonParser = bodyParser.json();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+		skip: (req, res) => process.env.NODE_ENV === 'test',
+	})
 );
 
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
-
+// app.use(
+// 	cors({
+// 		origin: CLIENT_ORIGIN,
+// 	})
+// );
 
 //image post
 app.use(jsonParser);
- 
+
 passport.use(localStrategy);
 passport.use(jwtStrategy);
- 
+
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/games', gameRouter);
@@ -46,19 +45,19 @@ app.use('/image-upload', imageRouter);
 app.use('/email', emailRouter);
 
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+	const server = app
+		.listen(port, () => {
+			console.info(`App listening on port ${server.address().port}`);
+		})
+		.on('error', (err) => {
+			console.error('Express failed to start');
+			console.error(err);
+		});
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+	dbConnect();
+	runServer();
 }
 
 module.exports = { app };
